cf9875a5f445c5327f8a2dcfc5a7b606edece0f0
Ihor <ihorrykun@gmail.com>
2023-02-05 14:29:05 +0200

    Update server.js

--- server.js
+++ server.js
@@ -19,6 +19,7 @@ const app = express();
 app.use(express.json());
 app.use(errorHandler);
 app.use(cors());
+
 app.post("/register", async (req, res) => {
   try {
     const { first_name, last_name, email, password } = req.body;
@@ -53,6 +54,35 @@ app.post("/register", async (req, res) => {
     user.token = token;
 
     res.status(201).json(user);
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+app.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    if (!(email && password)) {
+      res.status(400).send("All input is required");
+    }
+
+    const user = await User.findOne({ email });
+
+    if (user && (await bcrypt.compare(password, user.password))) {
+      const token = jwt.sign(
+        { user_id: user._id, email },
+        process.env.TOKEN_KEY,
+        {
+          expiresIn: "2h"
+        }
+      );
+
+      user.token = token;
+
+      res.status(200).json(user);
+    }
+    res.status(400).send("Invalid Credentials");
   } catch (err) {
     console.log(err);
   }
