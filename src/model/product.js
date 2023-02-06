c0e7dc12825096c9c6f870fa36fd398e1f4d87b3
Ihor <ihorrykun@gmail.com>
2023-02-06 15:26:02 +0200

    Update product.js

--- src/model/product.js
+++ src/model/product.js
@@ -1,10 +1,25 @@
 const { Schema, model } = require("mongoose");
 
 const ProductsSchema = new Schema({
-  category: String,
-  name: String,
-  color: String,
-  price: String
+  category: {
+    type: String,
+    require: true,
+    maxlength: 30
+  },
+  name: {
+    tupe: String,
+    require: true,
+    maxlength: 20
+  },
+  color: {
+    tupe: String,
+    require: true
+  },
+  price: {
+    type: Number,
+    require: true,
+    min: 0.01
+  }
 });
 
 const ProductsModel = model("Product", ProductsSchema);
